f1fd8ac9f2b7a7ab9cb66db04061b7f76b9aa931
ritatanght <doggymint@gmail.com>
2024-01-28 21:55:49 -0500

    Add background image to view

--- 01-weather/App.js
+++ 01-weather/App.js
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ImageBackground,
+} from 'react-native';
+import getImageForWeather from './utils/getImageForWeather';
 import SearchInput from './components/SearchInput';
 
 export default function App() {
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-      <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-      <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+      <ImageBackground
+        source={getImageForWeather('Clear')}
+        style={styles.imageContainer}
+        imageStyle={styles.image}
+      >
+        <View style={styles.detailsContainer}>
+          <Text style={[styles.largeText, styles.textStyle]}>
+            San Francisco
+          </Text>
+          <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
+          <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
 
-      <SearchInput placeholder="Search any city" />
+          <SearchInput placeholder="Search any city" />
+        </View>
+      </ImageBackground>
     </KeyboardAvoidingView>
   );
 }
@@ -17,18 +35,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   textStyle: {
     textAlign: 'center',
     // platform is a built-in React Native API that we need to import
     fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
   },
   largeText: {
     fontSize: 44,
   },
   smallText: {
     fontSize: 18,
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
   },
 });
