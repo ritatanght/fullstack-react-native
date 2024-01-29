d41b173e0fe9250058eb5fe71f1042cada866e41
ritatanght <doggymint@gmail.com>
2024-01-28 21:13:32 -0500

    Add TextInput and its styles

--- 01-weather/App.js
+++ 01-weather/App.js
@@ -1,13 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
       <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
       <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
       <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
-    </View>
+
+      <TextInput
+        autoCorrect={false}
+        placeholder="Search any city"
+        placeholderTextColor="white"
+        style={styles.textInput}
+        clearButtonMode="always"
+      />
+    </KeyboardAvoidingView>
   );
 }
 
@@ -28,5 +41,15 @@ const styles = StyleSheet.create({
   },
   smallText: {
     fontSize: 18,
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
   },
 });
