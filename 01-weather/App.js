55d2d28ca0fea5f0df9768342d9d26282ddaec24
ritatanght <doggymint@gmail.com>
2024-01-28 21:34:52 -0500

    Move the TextInput to SearchInput component

--- 01-weather/App.js
+++ 01-weather/App.js
@@ -1,10 +1,5 @@
-import {
-  StyleSheet,
-  Text,
-  KeyboardAvoidingView,
-  Platform,
-  TextInput,
-} from 'react-native';
+import { StyleSheet, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import SearchInput from './components/SearchInput';
 
 export default function App() {
   return (
@@ -13,13 +8,7 @@ export default function App() {
       <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
       <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
 
-      <TextInput
-        autoCorrect={false}
-        placeholder="Search any city"
-        placeholderTextColor="white"
-        style={styles.textInput}
-        clearButtonMode="always"
-      />
+      <SearchInput placeholder="Search any city" />
     </KeyboardAvoidingView>
   );
 }
@@ -41,15 +30,5 @@ const styles = StyleSheet.create({
   },
   smallText: {
     fontSize: 18,
-  },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height: 40,
-    width: 300,
-    marginTop: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center',
   },
 });
