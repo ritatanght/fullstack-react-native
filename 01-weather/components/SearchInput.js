e3464df8cbe13f83b145cbf8b3eafe0ce5d6bcdf
ritatanght <doggymint@gmail.com>
2024-01-29 14:17:53 -0500

    Add text state and functions for changing and submitting the text

--- 01-weather/components/SearchInput.js
+++ 01-weather/components/SearchInput.js
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 
-export default function SearchInput({ placeholder }) {
+export default function SearchInput({ placeholder, onSubmit }) {
+  const [text, setText] = useState('');
+
+  const handleChangeText = (text) => setText(text);
+
+  const handleSubmitEditing = () => {
+    if (!text) return;
+    onSubmit(text);
+    setText('');
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
         autoCorrect={false}
+        value={text}
         placeholder={placeholder}
         placeholderTextColor="white"
         underlineColorAndroid="transparent"
         style={styles.textInput}
         clearButtonMode="always"
+        onChangeText={handleChangeText}
+        onSubmitEditing={handleSubmitEditing}
       />
     </View>
   );
