a41fe409c01230562df8f79600a6fc7345784151
ritatanght <doggymint@gmail.com>
2024-01-30 18:08:58 -0500

    Add isOpen state and function to open form

--- 02-time-tracking/components/ToggleableTimerForm.js
+++ 02-time-tracking/components/ToggleableTimerForm.js
@@ -1,10 +1,19 @@
 import { StyleSheet, View } from "react-native";
 import TimerButton from "./TimerButton";
 import TimerForm from "./TimerForm";
-export default function ToggleableTimerForm({ isOpen }) {
+import { useState } from "react";
+export default function ToggleableTimerForm() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleFormOpen = () => setIsOpen(true);
+
   return (
     <View style={[styles.container, !isOpen && styles.buttonPadding]}>
-      {isOpen ? <TimerForm /> : <TimerButton title="+" color="black" />}
+      {isOpen ? (
+        <TimerForm />
+      ) : (
+        <TimerButton title="+" color="black" onPress={handleFormOpen} />
+      )}
     </View>
   );
 }
