916afe5dd9817abc5b0e48b17d407eb4cd78a3d8
ritatanght <doggymint@gmail.com>
2024-01-30 14:23:59 -0500

    Fix typo

--- 02-time-tracking/components/TimerForm.js
+++ 02-time-tracking/components/TimerForm.js
@@ -1,5 +1,5 @@
 import { StyleSheet, View, Text, TextInput } from "react-native";
-import TimeButton from "./TimerButton";
+import TimerButton from "./TimerButton";
 
 export default function TimerForm({ id, title, project }) {
   const submitText = id ? "Update" : "Create";
