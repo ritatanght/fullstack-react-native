5f324f372c73bbe5ee3ba1f1fe1a5d231aa6c4f0
ritatanght <doggymint@gmail.com>
2024-01-30 20:41:20 -0500

    Add functions for handling form submission and closing

--- 02-time-tracking/components/ToggleableTimerForm.js
+++ 02-time-tracking/components/ToggleableTimerForm.js
@@ -2,15 +2,23 @@ import { StyleSheet, View } from "react-native";
 import TimerButton from "./TimerButton";
 import TimerForm from "./TimerForm";
 import { useState } from "react";
-export default function ToggleableTimerForm() {
+export default function ToggleableTimerForm({ onFormSubmit }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleFormOpen = () => setIsOpen(true);
+  const handleFormClose = () => setIsOpen(false);
+  const handleFormSubmit = (timer) => {
+    onFormSubmit(timer);
+    setIsOpen(false);
+  };
 
   return (
     <View style={[styles.container, !isOpen && styles.buttonPadding]}>
       {isOpen ? (
-        <TimerForm />
+        <TimerForm
+          onFormSubmit={handleFormSubmit}
+          onFormClose={handleFormClose}
+        />
       ) : (
         <TimerButton title="+" color="black" onPress={handleFormOpen} />
       )}
