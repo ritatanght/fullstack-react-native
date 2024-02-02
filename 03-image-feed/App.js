7181d35a39884edcc389def6b42054c45d82084f
ritatanght <doggymint@gmail.com>
2024-02-02 17:06:27 -0500

    Adjust styles on container and add statusbar constants height

--- 03-image-feed/App.js
+++ 03-image-feed/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from "expo-constants";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Avatar from "./components/Avatar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Avatar backgroundColor={"teal"} initials="TT" size={35} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
