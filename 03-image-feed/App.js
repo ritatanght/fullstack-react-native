f291c2333305ce9357a310959958781bd85d923e
ritatanght <doggymint@gmail.com>
2024-02-03 17:39:25 -0500

    Add Feed  to App

--- 03-image-feed/App.js
+++ 03-image-feed/App.js
@@ -1,24 +1,30 @@
 import Constants from "expo-constants";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import CardList from "./components/CardList";
-const items = [
-  { id: 0, author: "Bob Ross" },
-  { id: 1, author: "Chuck Norris" },
-];
+import { Platform, StyleSheet, View } from "react-native";
+import Feed from "./screens/Feed";
+
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <CardList items={items} />
+      <Feed style={styles.feed} />
     </View>
   );
 }
 
+const platformVersion =
+  Platform.OS === "ios" ? parseInt(Platform.Version, 10) : Platform.Version;
+
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: "#fff",
+  },
+  feed: {
+    flex: 1,
+    marginTop:
+      Platform.OS === "android" || platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0,
   },
 });
