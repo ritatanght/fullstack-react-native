f10bd9466dcdda1bda81b9fb9f0b34af245c9b17
ritatanght <doggymint@gmail.com>
2024-02-03 18:51:29 -0500

    Add state function for textInput

--- 03-image-feed/components/CommentInput.js
+++ 03-image-feed/components/CommentInput.js
@@ -1,7 +1,20 @@
 import { StyleSheet, TextInput, View } from "react-native";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-const CommentInput = ({ onSubmit, placeholder }) => {};
+const CommentInput = ({ onSubmit, placeholder }) => {
+  const [text, setText] = useState("");
+
+  const handleChangeText = (text) => setText(text);
+
+  return (
+    <TextInput
+      value={text}
+      onChangeText={handleChangeText}
+      placeholder={placeholder}
+    />
+  );
+};
 
 CommentInput.PropTypes = {
   onSubmit: PropTypes.func.isRequired,
