18671d97df3aaa6f88cf625e6acfc0ea7d8982c8
ritatanght <doggymint@gmail.com>
2024-02-04 17:00:14 -0500

    Add message creation functions for text, image, and location

--- 04-messaging/utils/MessageUtils.js
+++ 04-messaging/utils/MessageUtils.js
@@ -11,3 +11,34 @@ export const MessageShape = PropTypes.shape({
     longitude: PropTypes.number.isRequired,
   }),
 });
+
+let messageId = 0;
+
+const getNextId = () => {
+  messageId += 1;
+  return messageId;
+};
+
+export const createTextMessage = (text) => {
+  return {
+    type: 'text',
+    id: getNextId(),
+    text,
+  };
+};
+
+export const createImageMessage = (uri) => {
+  return {
+    type: 'image',
+    id: getNextId(),
+    uri,
+  };
+};
+
+export const createLocationMessage = (coordinate) => {
+  return {
+    type: 'location',
+    id: getNextId(),
+    coordinate,
+  };
+};
