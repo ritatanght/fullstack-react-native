8ebdbf774b3fbc4790f74963c7234fc2be258284
ritatanght <doggymint@gmail.com>
2024-02-03 20:50:12 -0500

    Add CommentList component to display list of comments

---
+++ 03-image-feed/components/CommentList.js
@@ -0,0 +1,27 @@
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import PropTypes from "prop-types";
+
+const CommentList = ({ items }) => {
+  const renderItem = (item, index) => (
+    <View key={index} style={styles.comment}>
+      <Text>{item}</Text>
+    </View>
+  );
+  return <ScrollView>{items.map(renderItem)}</ScrollView>;
+};
+
+const styles = StyleSheet.create({
+  comment: {
+    marginLeft: 20,
+    paddingVertical: 20,
+    paddingRight: 20,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "rgba(0,0,0,0.05)",
+  },
+});
+
+CommentList.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default CommentList;
