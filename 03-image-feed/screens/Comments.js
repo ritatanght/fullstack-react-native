d73f72ffcbe7cf6a683730590e9df821693a80f9
ritatanght <doggymint@gmail.com>
2024-02-03 21:03:52 -0500

    Add Comments screen

---
+++ 03-image-feed/screens/Comments.js
@@ -0,0 +1,34 @@
+import { SafeAreaView } from "react-native";
+import { ViewPropTypes } from "deprecated-react-native-prop-types";
+import PropTypes from "prop-types";
+
+import CommentInput from "../components/CommentInput";
+import CommentList from "../components/CommentList";
+import NavigationBar from "../components/NavigationBar";
+
+const Comments = ({ style, comments, onClose, onSubmitComment }) => {
+  return (
+    <SafeAreaView style={style}>
+      <NavigationBar
+        title="Comments"
+        leftText="Close"
+        onPressLeftText={onClose}
+      />
+      <CommentInput placeholder="Leave a comment" onSubmit={onSubmitComment} />
+      <CommentList items={comments} />
+    </SafeAreaView>
+  );
+};
+
+Comments.propTypes = {
+  style: StyleSheet.ViewPropTypes,
+  comments: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onClose: PropTypes.func.isRequired,
+  onSubmitComment: PropTypes.func.isRequired,
+};
+
+Comments.defaultProps = {
+  style: null,
+};
+
+export default Comments;
