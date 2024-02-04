feea2a04ca68e36c5d82c673b1f1216dabd16a8e
ritatanght <doggymint@gmail.com>
2024-02-04 13:26:57 -0500

    Add starter files for messaging

---
+++ 04-messaging/App.js
@@ -0,0 +1,39 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function App() {
+  const renderMessageList = () => <View style={styles.content}></View>;
+
+  const renderInputMethodEditor = () => (
+    <View style={styles.inputMethodEditor}></View>
+  );
+
+  const renderToolbar = () => <View style={styles.toolbar}></View>;
+
+  return (
+    <View style={styles.container}>
+      {renderMessageList()}
+      {renderToolbar()}
+      {renderInputMethodEditor()}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0,0.04)',
+    backgroundColor: '#fff',
+  },
+});
