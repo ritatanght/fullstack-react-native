da762686783d504271ff595c7daccf82fb0c7fe5
ritatanght <doggymint@gmail.com>
2024-02-04 17:24:08 -0500

    Add styles and imports

--- 04-messaging/components/MessageList.js
+++ 04-messaging/components/MessageList.js
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
 import { MessageShape } from '../utils/MessageUtils';
-import { FlatList, StyleSheet } from 'react-native';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { MapView } from 'expo'; // comes from react-native-maps
 
 const keyExtractor = (item) => item.id.toString();
 const MessageList = ({ messages, onPressMessage }) => {
   const renderMessageItem = ({ item }) => {};
+
   return (
     <FlatList
       style={styles.container}
       inverted
-      data={message}
+      data={messages}
       renderItem={renderMessageItem}
       keyExtractor={keyExtractor}
       keyboardShouldPersistTaps={'handled'}
@@ -21,6 +30,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     overflow: 'visible', // prevents clipping on resize
+  },
+  messageRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginBottom: 4,
+    marginRight: 10,
+    marginLeft: 60,
+  },
+  messageBubble: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: 'rgb(16,135,255)',
+    borderRadius: 20,
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 10,
+  },
+  map: {
+    width: 250,
+    height: 250,
+    borderRadius: 10,
   },
 });
 
