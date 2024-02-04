4df24902c6a1c1f00372b527108a33b46e9a464c
ritatanght <doggymint@gmail.com>
2024-02-03 21:41:32 -0500

    Add comments to CardList component and update PropTypes

--- 03-image-feed/components/CardList.js
+++ 03-image-feed/components/CardList.js
@@ -6,15 +6,25 @@ import Card from "./Card";
 
 const keyExtractor = ({ id }) => id.toString();
 
-const CardList = ({ items }) => {
-  const renderItem = ({ item: { id, author } }) => (
-    <Card fullname={author} image={{ uri: getImageFromId(id) }} />
-  );
+const CardList = ({ items, commentsForItem, onPressComments }) => {
+  const renderItem = ({ item: { id, author } }) => {
+    const comments = commentsForItem[id];
+    return (
+      <Card
+        fullname={author}
+        image={{ uri: getImageFromId(id) }}
+        linkText={`${comments ? comments.length : 0} Comments`}
+        onPressLinkText={() => onPressComments(id)}
+      />
+    );
+  };
+
   return (
     <FlatList
       data={items}
       renderItem={renderItem}
       keyExtractor={keyExtractor}
+      extraData={commentsForItem}
     />
   );
 };
@@ -28,6 +38,9 @@ CardList.PropTypes = {
       author: PropTypes.string.isRequired,
     })
   ).isRequired,
+  commentsForItem: PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string))
+    .isRequired,
+  onPressComments: PropTypes.func.isRequired,
 };
 
 export default CardList;
