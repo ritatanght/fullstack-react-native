b6719da362335f4a7c42382b92d03a2f554c1a35
ritatanght <doggymint@gmail.com>
2024-02-04 17:13:43 -0500

    Add FlatList and keyExtractor, and styles for container

--- 04-messaging/components/MessageList.js
+++ 04-messaging/components/MessageList.js
@@ -2,7 +2,27 @@ import PropTypes from 'prop-types';
 import { MessageShape } from '../utils/MessageUtils';
 import { FlatList, StyleSheet } from 'react-native';
 
-const MessageList = ({ messages, onPressMessage }) => {};
+const keyExtractor = (item) => item.id.toString();
+const MessageList = ({ messages, onPressMessage }) => {
+  const renderMessageItem = ({ item }) => {};
+  return (
+    <FlatList
+      style={styles.container}
+      inverted
+      data={message}
+      renderItem={renderMessageItem}
+      keyExtractor={keyExtractor}
+      keyboardShouldPersistTaps={'handled'}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    overflow: 'visible', // prevents clipping on resize
+  },
+});
 
 MessageList.propTypes = {
   messages: PropTypes.arrayOf(MessageShape).isRequired,
