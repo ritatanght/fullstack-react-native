a22f3a6e52c68147319c54de947e411062b93296
ritatanght <doggymint@gmail.com>
2024-02-03 20:57:41 -0500

    Add UI and styles for NavigationBar

--- 03-image-feed/components/NavigationBar.js
+++ 03-image-feed/components/NavigationBar.js
@@ -1,9 +1,36 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import PropTypes from "prop-types";
 
-const NavigationBar = ({ title, leftText, onPressLeftText }) => {};
+const NavigationBar = ({ title, leftText, onPressLeftText }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.leftText} onPress={onPressLeftText}>
+        <Text>{leftText}</Text>
+      </TouchableOpacity>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
 
-export default NavigationBar;
+const styles = StyleSheet.create({
+  container: {
+    height: 40,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "rgba(0,0,0,0.1)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "500",
+  },
+  leftText: {
+    position: "absolute",
+    left: 20,
+    top: 0,
+    bottom: 0,
+    justifyContent: "center",
+  },
+});
 
 NavigationBar.propTypes = {
   title: PropTypes.string,
@@ -16,3 +43,5 @@ NavigationBar.defaultProps = {
   leftText: "",
   onPressLeftText: () => {},
 };
+
+export default NavigationBar;
