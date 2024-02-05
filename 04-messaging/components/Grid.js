acc3f5c24f4f4a7e7e3fb2ab8380ceedf210cbaa
ritatanght <doggymint@gmail.com>
2024-02-05 16:43:58 -0500

    Refactor Grid component to be a custom FlatList component

--- 04-messaging/components/Grid.js
+++ 04-messaging/components/Grid.js
@@ -2,7 +2,21 @@ import { Dimensions, FlatList, PixelRatio, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
 const Grid = (props) => {
-  const renderGridItem = (info) => {};
+  const renderGridItem = (info) => {
+    const { renderItem, numColumns, itemMargin } = props;
+    const { width } = Dimensions.get('window');
+    const size = PixelRatio.roundToNearestPixel(
+      (width - itemMargin * (numColumns - 1)) / numColumns,
+    );
+
+    // We don't want to include a `marginLeft` on the first item of a row
+    const marginLeft = index % numColumns === 0 ? 0 : itemMargin;
+
+    // We don't want to include a `marginTop` on the first row of the grid
+    const marginTop = index < numColumns ? 0 : itemMargin;
+
+    return renderItem({ ...info, size, marginLeft, marginTop });
+  };
 
   return <FlatList {...props} renderItem={renderGridItem} />;
 };
